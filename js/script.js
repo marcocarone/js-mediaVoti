2122cdb464f7c5b9aa754e0e07aaa36fd58c9227
Marco <marco.carone@libero.it>
2020-01-12 16:43:06 +0100

    fix js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 // è richiesto di inserire tre voti, con questi si farà la somma e successivamente la media. infine si controllerà se la media è maggiore o minore di 6 dando i relativi output
 
-
+var somma = 0;
 for (var i = 1; i <= 3; i++) {
   var votiInput = parseInt(prompt("Inserisci il voto"));
+  console.log("voto " + votiInput);
   while (isNaN(votiInput)) {
     votiInput = parseInt(prompt("Inserisci il voto"));
   }
-  var somma = 0;
   somma += votiInput;
-  console.log(somma);
+  console.log("somma: " + somma);
   var media = somma / i;
-  console.log(media);
+  console.log("media: " + media);
 }
+
 var risultato = "la media dei voti è minore di 6";
 if (media < 6) {
   document.getElementById("azione").innerHTML = risultato;
