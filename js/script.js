b91a080aa34845697547cdbe95afc5062ea83b1d
Marco <marco.carone@libero.it>
2020-01-12 19:09:19 +0100

    fixed

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ for (var i = 1; i <= 3; i++) {
   while (isNaN(votiInput)) {
     votiInput = parseInt(prompt("Inserisci il voto"));
   }
+  document.getElementById("numeri").innerHTML += "<li>" + votiInput + "</li>";
   somma += votiInput;
   console.log("somma: " + somma);
   var media = somma / i;
@@ -23,3 +24,6 @@ if (media < 6) {
   risultato = "la media dei voti è maggiore di 6";
   document.getElementById("azione").innerHTML = risultato;
 }
+
+document.getElementById("somma").innerHTML = "La somma dei 3 numeri inseriti dall'utente è: " + somma;
+document.getElementById("media").innerHTML = "La media dei 3 numeri inseriti dall'utente è: " + media.toFixed(2);
